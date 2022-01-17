260d71a47b29ab3e3729c11fea5078ff2d7650d2
snyderae13 <snyderae13@icloud.com>
2022-01-17 12:03:21 -0600

    id passed and routes fixed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
-import './App.css';
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
+import './App.css'
 
 function App() {
-  let [searchTerm, setSearchTerm] = useState('')
-  let [data, setData] = useState([])
-  let [message, setMessage] = useState('Search for Music!')
+let [searchTerm, setSearchTerm] = useState('')
+let [data, setData] = useState([])
+let [message, setMessage] = useState('Search for Music!')
 
-  useEffect(() => {
+
+useEffect(() => {
+    const API_URL = `https://itunes.apple.com/search?term=`
     if (searchTerm) {
-      document.title=`${searchTerm} Music`
-      const fetchData = async () => {
-        const response = await fetch(`https://itunes.apple.com/search?term=${searchTerm}`)
+    document.title=`${searchTerm} Music`
+    const fetchData = async () => {
+        const response = await fetch(API_URL + searchTerm)
         const resData = await response.json()
         if(resData.results.length > 0) {
-          setData(resData.results)
+        setData(resData.results)
         } else {
-          setMessage('Not Found')
+        setMessage('Not Found')
         }
-      }
-      fetchData()
-  }
-  }, [searchTerm])
+    }
+    fetchData()
+}
+}, [searchTerm])
 
-  const handleSearch = (e, term) => {
+const handleSearch = (e, term) => {
     e.preventDefault()
     setSearchTerm(term)
-  }
+}
 
-  return (
-    <div className="App">
-      <SearchBar handleSearch={handleSearch} />
-      {message}
-      <Gallery data={data} />
-    </div>
-  );
+return (
+  <div className="App">
+    {message}
+    <Router>
+      <Routes>
+      <Route exact path="/" element={<><SearchBar handleSearch={handleSearch} /><Gallery data={data} /></>}>
+        
+      </Route>
+      <Route path="/album/:id" element={<AlbumView term={searchTerm} />}>
+        
+      </Route>
+      <Route path="/artist/:id" element={<ArtistView term={searchTerm} />}>
+        
+      </Route>
+      </Routes>
+    </Router>
+  </div>
+);
 }
 
 export default App;
